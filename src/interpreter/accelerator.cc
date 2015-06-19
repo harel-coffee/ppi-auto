e1a8137ada293c18f7acf6d31ee41bc51d547f8e
Amanda S. Dufek <amanda.dufek@gmail.com>
2015-06-19 14:29:44 -0300

    Alguns ajustes.

--- src/interpreter/accelerator.cc
+++ src/interpreter/accelerator.cc
@@ -236,7 +236,7 @@ const char* kernel_str  =
    "      } "
    "      else "
    "      { "
-   "         vector[gr_id * local_size + lo_id] = pilha[topo]; "
+   "         vector[gl_id] = pilha[topo]; "
    "      } "
    "   } "
    "   if( !mode ) " 
@@ -272,59 +272,81 @@ void acc_interpret_init( const unsigned size, float** input, float** model, floa
    if( !strcmp(type,"CPU") ) {acc = CL_DEVICE_TYPE_CPU;}
    else {acc = CL_DEVICE_TYPE_GPU;}
 
-   // Descobrir e escolher as plataformas
-   vector<cl::Platform> plataformas;
-   // Descobrir os dispositivos
-   vector<cl::Device> dispositivos;
-
    // Descobre as plataformas instaladas no hospedeiro
+   vector<cl::Platform> plataformas;
    cl::Platform::get( &plataformas );
 
-   plataformas[0].getDevices( CL_DEVICE_TYPE_ALL, &dispositivos );
+   vector<cl::Device> dispositivo(1); 
+   int device_type;
 
-//   vector<cl::Device> dispositivo(1);
-//   for( int m = 0; m < plataformas.size(); m++ )
-//   {
-//     // Descobrir os dispositivos
-//     vector<cl::Device> dispositivos;
-//     // Descobre os dispositivos da plataforma m
-//     plataformas[m].getDevices( CL_DEVICE_TYPE_ALL, &dispositivos );
-//     for ( int n = 0; n < dispositivos.size(); n++ )
-//     {
-//        if ( dispositivos[n].getInfo<CL_DEVICE_TYPE>() == acc ) 
-//         dispositivo[0] = dispositivos[n];
-//     }
-//   }
+   bool leave = false;
+   for( int m = 0; m < plataformas.size(); m++ )
+   {
+      vector<cl::Device> dispositivos;
+      // Descobre os dispositivos da plataforma m
+      plataformas[m].getDevices( CL_DEVICE_TYPE_ALL, &dispositivos );
+
+      dispositivo[0] = dispositivos[0];
+      device_type = dispositivo[0].getInfo<CL_DEVICE_TYPE>();
+      for ( int n = 0; n < dispositivos.size(); n++ )
+      {
+         if ( dispositivos[n].getInfo<CL_DEVICE_TYPE>() == acc ) 
+         {
+            leave = true;
+            dispositivo[0] = dispositivos[n];
+            device_type = dispositivo[0].getInfo<CL_DEVICE_TYPE>();
+            break;
+         }
+      }
+      if( leave ) break;
+   }
 
    // Criar o contexto
-   //data.context = cl::Context( dispositivo );
-   data.context = cl::Context( dispositivos );
+   data.context = cl::Context( dispositivo );
 
    // Criar a fila de comandos para um dispositivo (aqui só o primeiro)
    //data.fila = cl::CommandQueue( data.context, dispositivo[0], CL_QUEUE_PROFILING_ENABLE );
-   data.fila = cl::CommandQueue( data.context, dispositivos[0], CL_QUEUE_PROFILING_ENABLE );
+   data.fila = cl::CommandQueue( data.context, dispositivo[0], CL_QUEUE_PROFILING_ENABLE );
 
    data.buffer_inputs = cl::Buffer( data.context, CL_MEM_READ_ONLY | CL_MEM_ALLOC_HOST_PTR, nlin * ncol * sizeof( float ) );
 
    float* inputs = (float*) data.fila.enqueueMapBuffer( data.buffer_inputs, CL_TRUE, CL_MAP_WRITE, 0, nlin * ncol * sizeof( float ) );
 
-   for( int i = 0; i < nlin; i++ )
-   {
-      for( int j = 0; j < ninput; j++ )
-      {
-         inputs[i * ncol + j] = input[i][j];
-      }
-      for( int j = ninput; j < (nmodel + ninput); j++ )
-      {
-         inputs[i * ncol + j] = model[i][j];
-      }
-      inputs[i * ncol + (nmodel + ninput)] = obs[i];
-   }
 
+//   if ( device_type == CL_DEVICE_TYPE_CPU ) 
+//   {
+//      for( int i = 0; i < nlin; i++ )
+//      {
+//         for( int j = 0; j < ninput; j++ )
+//         {
+//            inputs[i * ncol + j] = input[i][j];
+//         }
+//         for( int j = ninput; j < (nmodel + ninput); j++ )
+//         {
+//            inputs[i * ncol + j] = model[i][j];
+//         }
+//         inputs[i * ncol + (nmodel + ninput)] = obs[i];
+//      }
+//   }
+//   else
+//   {
+      for( int i = 0; i < nlin; i++ )
+      {
+         for( int j = 0; j < ninput; j++ )
+         {
+            inputs[j * nlin + i] = input[i][j];
+         }
+         for( int j = ninput; j < (nmodel + ninput); j++ )
+         {
+            inputs[j * nlin + i] = model[i][j];
+         }
+         inputs[(nmodel + ninput) * nlin + i] = obs[i];
+      }
+//   }
+ 
    // Unmapping
    data.fila.enqueueUnmapMemObject( data.buffer_inputs, inputs ); 
 
-//   ////////
 //   inputs = (float*) data.fila.enqueueMapBuffer( data.buffer_inputs, CL_TRUE, CL_MAP_READ, 0, nlin * ncol * sizeof( float ) );
 //   for( int i = 0; i < nlin*ncol; i++ )
 //   {
@@ -333,7 +355,6 @@ void acc_interpret_init( const unsigned size, float** input, float** model, floa
 //   }
 //   // Unmapping
 //   data.fila.enqueueUnmapMemObject( data.buffer_inputs, inputs ); 
-   ////////
 
    // Carregar o programa, compilá-lo e gerar o kernel
    cl::Program::Sources fonte( 1, make_pair( kernel_str, strlen( kernel_str ) ) );
@@ -344,12 +365,11 @@ void acc_interpret_init( const unsigned size, float** input, float** model, floa
    char buildOptions[60];
    sprintf( buildOptions, "-DTAM_MAX=%u -DTERMINAL_MIN=%u", size, TERMINAL_MIN );  
    try {
-      programa.build( dispositivos, buildOptions );
-   } 
+      programa.build( dispositivo, buildOptions );
+   }
    catch( cl::Error )
    {
-     //cout << "Build Log:\t " << programa.getBuildInfo<CL_PROGRAM_BUILD_LOG>(dispositivo[0]) << std::endl;
-     cout << "Build Log:\t " << programa.getBuildInfo<CL_PROGRAM_BUILD_LOG>(dispositivos[0]) << std::endl;
+     cout << "Build Log:\t " << programa.getBuildInfo<CL_PROGRAM_BUILD_LOG>(dispositivo[0]) << std::endl;
    }
 
    // Cria a variável kernel que vai representar o kernel "avalia"
@@ -358,8 +378,14 @@ void acc_interpret_init( const unsigned size, float** input, float** model, floa
    // 2) Preparação da memória dos dispositivos (leitura e escrita}
    data.buffer_phenotype = cl::Buffer( data.context, CL_MEM_READ_ONLY, size * sizeof( Symbol ) );
    data.buffer_ephemeral = cl::Buffer( data.context, CL_MEM_READ_ONLY, size * sizeof( float ) );
-   if( mode ) {data.buffer_vector = cl::Buffer( data.context, CL_MEM_WRITE_ONLY | CL_MEM_ALLOC_HOST_PTR, data.nlin * sizeof( float ) );}
-   else {data.buffer_vector = cl::Buffer( data.context, CL_MEM_WRITE_ONLY | CL_MEM_ALLOC_HOST_PTR, (data.global_size/data.local_size) * sizeof( float ) );} 
+   if( mode ) 
+   { 
+      data.buffer_vector = cl::Buffer( data.context, CL_MEM_WRITE_ONLY | CL_MEM_ALLOC_HOST_PTR, data.nlin * sizeof( float ) );
+   }
+   else 
+   {
+      data.buffer_vector = cl::Buffer( data.context, CL_MEM_WRITE_ONLY | CL_MEM_ALLOC_HOST_PTR, (data.global_size/data.local_size) * sizeof( float ) );
+   } 
 
    // Execução do kernel: definição dos argumentos e trabalho/particionamento
    data.kernel.setArg( 0, data.buffer_phenotype );
@@ -386,7 +412,12 @@ void acc_interpret( Symbol* phenotype, float* ephemeral, int size, float* vector
    data.fila.finish();
 
    // Transferência dos resultados para o hospedeiro 
-   if ( !mode )
+   if ( mode )
+   {
+      float* tmp = (float*) data.fila.enqueueMapBuffer( data.buffer_vector, CL_TRUE, CL_MAP_READ, 0, data.nlin * sizeof( float ) );
+      for( int i = 0; i < data.nlin; i++ ) {vector[i] = tmp[i];}
+   }
+   else
    {
       float* tmp = (float*) data.fila.enqueueMapBuffer( data.buffer_vector, CL_TRUE, CL_MAP_READ, 0, (data.global_size/data.local_size) * sizeof( float ) );
       float sum = 0.0;
@@ -397,9 +428,5 @@ void acc_interpret( Symbol* phenotype, float* ephemeral, int size, float* vector
 
       if( isnan( sum ) || isinf( sum ) ) {vector[0] = std::numeric_limits<float>::max();}
       else {vector[0] = sum/data.nlin;}
-   }
-   else
-   {
-      vector = (float*) data.fila.enqueueMapBuffer( data.buffer_vector, CL_TRUE, CL_MAP_READ, 0, data.nlin * sizeof( float ) );
    }
 }
