6d539987cfb4dbeba740270e539da2d2686b571c
Amanda S. Dufek <amanda.dufek@gmail.com>
2016-04-15 15:07:56 -0300

    Unificando o vetor de entrada (main.cc).

--- src/main.cc
+++ src/main.cc
@@ -12,7 +12,7 @@
 /** *********************** AUXILIARY FUNCTIONS ********************** **/
 /** ****************************************************************** **/
 
-int read( const std::string& dataset, float**& input, float**& model, float*& obs, int ninput, int nmodel, int& nlin, int prediction )
+int read( const std::string& dataset, float**& input, int ncol, int& nlin, int prediction )
 {
    char c;
    FILE *arqentra;
@@ -29,68 +29,40 @@ int read( const std::string& dataset, float**& input, float**& model, float*& ob
 
    input   = new float*[nlin];
    for( int i = 0; i < nlin; i++ )
-     input[i] = new float[ninput];
-   model   = new float*[nlin];
-   for( int i = 0; i < nlin; i++ )
-     model[i] = new float[nmodel];
-   obs   = new float[nlin]();
+     input[i] = new float[ncol];
 
    rewind(arqentra);
    for( int i = 0; i < nlin; i++ )
    {
-      for( int j = 0; j < ninput; j++ )
+      for( int j = 0; j < ncol; j++ )
       {
-         //fprintf(stdout,"%d ",fscanf(arqentra,"%f,",&input[i][j]));
-         //fprintf(stdout,"%f\n",input[i][j]);
-         if( fscanf(arqentra,"%f,",&input[i][j]) != 1 || isnan(input[i][j]) || isinf(input[i][j]) )
+         if( j == (ncol-1) )
          {
-            fprintf(stderr, "Invalid input at line %d and column %d.\n", i, j);
-            return 2;
-         }
-      }
-      for( int j = 0; j < nmodel; j++ )
-      {
-         if( prediction && j == (nmodel-1) )
-         {
-            if( fscanf(arqentra,"%f%*[^\n]",&model[i][j]) != 1 || isnan(model[i][j]) || isinf(model[i][j]) )
+            if( fscanf(arqentra,"%f%*[^\n],",&input[i][j]) != 1 || isnan(input[i][j]) || isinf(input[i][j]) )
             {
-               fprintf(stderr, "Invalid model at line %d and column %d.\n", i, j);
+               fprintf(stderr, "Invalid input at line %d and column %d.\n", i, j);
                return 2;
             }
          }
          else
          {
-            if( fscanf(arqentra,"%f,",&model[i][j]) != 1 || isnan(model[i][j]) || isinf(model[i][j]) )
+            if( fscanf(arqentra,"%f,",&input[i][j]) != 1 || isnan(input[i][j]) || isinf(input[i][j]) )
             {
-               fprintf(stderr, "Invalid model at line %d and column %d.\n", i, j);
+               fprintf(stderr, "Invalid input at line %d and column %d.\n", i, j);
                return 2;
             }
          }
-      }
-      if( !prediction )
-      {
-         if( fscanf(arqentra,"%f",&obs[i]) != 1 || isnan(obs[i]) || isinf(obs[i]) )
-            {
-               fprintf(stderr, "Invalid observation at line %d.\n", i);
-               return 2;
-            }
       }
    }
    fclose (arqentra);
    return 0;
 }
 
-void destroy( float** input, float** model, float* obs, int nlin )
+void destroy( float** input, int nlin )
 {
-   delete[] obs;
-
    for( int i = 0; i < nlin; ++i )
      delete [] input[i];
    delete [] input;
-
-   for( int i = 0; i < nlin; ++i )
-     delete [] model[i];
-   delete [] model;
 }
 
 
@@ -99,28 +71,24 @@ int main(int argc, char **argv)
    CmdLine::Parser Opts( argc, argv );
 
    Opts.Bool.Add( "-pred", "--prediction" );
-   Opts.Int.Add( "-ni", "--number_of_inputs" );
-   Opts.Int.Add( "-nm", "--number_of_models" );
+   Opts.Int.Add( "-ncol", "--number_of_columns" );
    Opts.Int.Add( "-port", "--number_of_port" );
    Opts.String.Add( "-d", "--dataset" );
    Opts.String.Add( "-run", "--program_file" );
 
    Opts.Process();
 
-   int ninput = Opts.Int.Get("-ni");   
-   int nmodel = Opts.Int.Get("-nm");   
+   int ncol = Opts.Int.Get("-ncol");   
    
    int nlin;
    float** input;
-   float** model;
-   float* obs;
 
-   int error = read( Opts.String.Get("-d"), input, model, obs, ninput, nmodel, nlin, Opts.Bool.Get("-pred") );
+   int error = read( Opts.String.Get("-d"), input, ncol, nlin, Opts.Bool.Get("-pred") );
    if ( error ) {return error;}
 
    if( Opts.String.Found("-run") )
    {
-      pep_init( input, model, obs, nlin, argc, argv );
+      pep_init( input, nlin, argc, argv );
       pep_interpret();
       pep_print( stdout );
       pep_destroy();
@@ -136,14 +104,14 @@ int main(int argc, char **argv)
       srv.start();
       //sleep(100);
 
-      pee_init( input, model, obs, nlin, argc, argv );
+      pee_init( input, nlin, argc, argv );
       pee_evolve();
       pee_print_best( stdout, 1 );
       pee_print_time();
       pee_destroy();
    }
   
-   destroy(input, model, obs, nlin);
+   destroy(input, nlin);
 
    return 0;
 }
