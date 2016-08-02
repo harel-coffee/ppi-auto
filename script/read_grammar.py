86d89faa970026fe9ce26f461d8ac776b1cabb1e
Douglas A. Augusto <daaugusto@gmail.com>
2016-08-02 12:40:02 -0300

    Added the following new functions:    CEIL FLOOR EXP EXP10 EXP2 LOG LOG10 LOG2 LOG LOG10 LOG2 SIN COS TAN TAN STEP SIGN LOGISTIC GAMMA GAUSS
    and the following new constants:
    
       PI PI_2 PI_4 1_PI 2_PI 2_SQRTPI SQRT2 SQRT1_2 E LOG2E LOG10E LN2 LN10
    
    See problem/random/grammar-full.bnf for a grammar that utilizes all functions and constants.

--- script/read_grammar.py
+++ script/read_grammar.py
@@ -213,6 +213,48 @@ f.close()
 symbol_head = r"""#ifndef __SYMBOL_H
 #define __SYMBOL_H
 
+#define UNPROTECTED_FUNCTIONS 1
+
+#ifndef M_PI_F
+   #define M_PI_F 3.14159274101257f
+#endif
+#ifndef M_PI_2_F
+   #define M_PI_2_F 1.57079637050629f
+#endif
+#ifndef M_PI_4_F
+   #define M_PI_4_F 0.78539818525314f
+#endif
+#ifndef M_1_PI_F
+   #define M_1_PI_F 0.31830987334251f
+#endif
+#ifndef M_2_PI_F
+   #define M_2_PI_F 0.63661974668503f
+#endif
+#ifndef M_2_SQRTPI_F
+   #define M_2_SQRTPI_F 1.12837922573090f
+#endif
+#ifndef M_SQRT2_F
+   #define M_SQRT2_F 1.41421353816986f
+#endif
+#ifndef M_SQRT1_2_F
+   #define M_SQRT1_2_F 0.70710676908493f
+#endif
+#ifndef M_E_F
+   #define M_E_F 2.71828174591064f
+#endif
+#ifndef M_LOG2E_F
+   #define M_LOG2E_F 1.44269502162933f
+#endif
+#ifndef M_LOG10E_F
+   #define M_LOG10E_F 0.43429449200630f
+#endif
+#ifndef M_LN2_F
+   #define M_LN2_F 0.69314718246460f
+#endif
+#ifndef M_LN10_F
+   #define M_LN10_F 2.30258512496948f
+#endif
+
 #define TERMINAL_MIN 10000
 #define ATTRIBUTE_MIN 20000
 
