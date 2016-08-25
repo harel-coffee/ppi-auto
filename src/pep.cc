05a06bd4a26e69c0894691e7d218252a318f1716
Amanda S. Dufek <amanda.dufek@gmail.com>
2016-08-25 14:32:24 -0300

    Trocando solution_file por solution.

--- src/pep.cc
+++ src/pep.cc
@@ -40,40 +40,48 @@ void pep_init( float** input, int nlin, int argc, char** argv )
    Opts.String.Add( "-error", "--function-difference", "fabs((X)-(Y))" );
    Opts.Int.Add( "-ncol", "--number_of_columns" );
    Opts.Process();
-   const char* file = Opts.String.Get("-sol").c_str();
+   
+   //const char* file = Opts.String.Get("-sol").c_str();
+   std::istringstream iss;
+   std::string solution = Opts.String.Get("-sol");
+   iss.str (solution);
+   std::cout << solution;
 
-
-   FILE *arqentra;
-   arqentra = fopen(file,"r");
-   if(arqentra == NULL) 
-   {
-     fprintf(stderr, "Could not open file for reading (%s).\n", file);
-     //printf("Nao foi possivel abrir o arquivo de entrada.\n");
-     exit(-1);
-   }
+   //FILE *arqentra;
+   //arqentra = fopen(file,"r");
+   //if(arqentra == NULL) 
+   //{
+   //  fprintf(stderr, "Could not open file for reading (%s).\n", file);
+   //  //printf("Nao foi possivel abrir o arquivo de entrada.\n");
+   //  exit(-1);
+   //}
    
    data.prediction = Opts.Bool.Get("-pred");
    data.version = Opts.Bool.Get("-acc");
 
    data.size = new int[1];
-   fscanf(arqentra,"%d",&data.size[0]);
-   fprintf(stdout,"%d\n",data.size[0]);
+   iss >> data.size[0];
+   //fscanf(arqentra,"%d",&data.size[0]);
+   //fprintf(stdout,"%d\n",data.size[0]);
 
    data.phenotype = new Symbol[data.size[0]];
    data.ephemeral = new float[data.size[0]];
 
    for( int tmp, i = 0; i < data.size[0]; ++i )
    {
-      fscanf(arqentra,"%d ",&tmp); data.phenotype[i] = (Symbol)tmp;
-      fprintf(stdout,"%d ",data.phenotype[i]);
+      iss >> tmp;
+      data.phenotype[i] = (Symbol)tmp;
+      //fscanf(arqentra,"%d ",&tmp); data.phenotype[i] = (Symbol)tmp;
+      //fprintf(stdout,"%d ",data.phenotype[i]);
       if( data.phenotype[i] == T_CONST || data.phenotype[i] == T_ATTRIBUTE )
       {
-         fscanf(arqentra,"%f ",&data.ephemeral[i]);
-         fprintf(stdout,"%.12f ",data.ephemeral[i]);
+         iss >> data.ephemeral[i];
+         //fscanf(arqentra,"%f ",&data.ephemeral[i]);
+         //fprintf(stdout,"%.12f ",data.ephemeral[i]);
       }
    }
-   fprintf(stdout,"\n");
-   fclose (arqentra);
+   //fprintf(stdout,"\n");
+   //fclose (arqentra);
 
    data.nlin = nlin;
 
