6f7a9bd1189c08fca99f2c530635a40e5dfdb4e6
Douglas A. Augusto <daaugusto@gmail.com>
2017-01-30 14:50:57 -0200

    Adds argparse.

--- script/sensibility_test.py
+++ script/sensibility_test.py
@@ -3,25 +3,11 @@
 
 import sys, random
 import numpy as np
-
-niter = 30
-scenarios = 100
+import argparse
 
 #random.seed(100)
 
-attrNames = ["X1", "X2", "X3"] 
-
-#exps = [['+', 'X1', '*', '3.14', '/', 'X2', 'X3'],['+', 'X1', '*', '3.14', '/', 'X2', 'X3'],['+', 'X1', '*', '3.14', '/', 'X2', 'X3']]
-
-exps = [
-['+', 'X1', '*', '3.14', '/', 'X2', 'X3'],
-['+', 'X1', '*', '3.14', '/', 'X3', 'X2'],
-['+', 'X2', '*', '3.14', '/', 'X1', 'X3'],
-['+', 'X2', '*', '3.14', '/', 'X3', 'X1'],
-['+', 'X3', '*', '3.14', '/', 'X1', 'X2'],
-['+', 'X3', '*', '3.14', '/', 'X2', 'X1'], ['+', 'X1', 'X1'], ['/', 'X1', '0.0']]
-
-#exps = [['+', '+', 'X1', 'X2', 'X3']]
+# -e '+ X1 * 3.14 / X2 X3' -e '+ X1 * 3.14 / X3 X2' -e '+ X2 * 3.14 / X1 X3' -e '+ X2 * 3.14 / X3 X1' -e '+ X3 * 3.14 / X1 X2' -e '+ X3 * 3.14 / X2 X1' -e '+ X1 X1' -e '/ X1 0.0'
 
 ################################################################################
 # Nice progress bar
@@ -37,7 +23,7 @@ def ProgressBar(count, total, suffix='', out=sys.stdout):
     out.flush()
 
 def get_sample(attrname):
-   return np.random.uniform(0.0,1.0) # TODO: uses a different distribution depending on the attribute
+   return np.random.uniform(args.min,args.max) # TODO: uses a different distribution depending on the attribute
 
 def print_stats(means, stds, attributes, nones, exps):
    stat_means = {}
@@ -53,7 +39,7 @@ def print_stats(means, stds, attributes, nones, exps):
    for i, attr in enumerate(attributes):
       if means[i]:
          frequency=len(means[i])
-         out += "%s: impact=%.2f%% (%.3f±%.1f), frequency=%.2f%% (%d/%d), none=%.2f%% (%d/%d)\n" % (attr, 100.*stat_means[i]/sum_stat_means, stat_means[i], stat_stds[i], 100. * frequency/float(len(exps)), frequency, len(exps), 100.*none[i]/float(niter*scenarios*len(exps)), none[i], niter*scenarios*len(exps))
+         out += "%s: impact=%.2f%% (%.3f±%.1f), frequency=%.2f%% (%d/%d), none=%.2f%% (%d/%d)\n" % (attr, 100.*stat_means[i]/sum_stat_means, stat_means[i], stat_stds[i], 100. * frequency/float(len(exps)), frequency, len(exps), 100.*none[i]/float(args.iterations*args.scenarios*len(exps)), none[i], args.iterations*args.scenarios*len(exps))
    sys.stdout.write(out)
 
 def interpreter(exp, attr):
@@ -159,6 +145,32 @@ def interpreter(exp, attr):
    return stack.pop()
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-e', '--exp', action='append', dest='exps', required=True, default=[], help="Set the expressions to be evaluated. Ex: -e '+ ATTR0 ATTR1' -e 'ATTR1'")
+parser.add_argument('-s', '--scenarios', required=False, type=int, default=100, help="The number of scenarios [default=100]")
+parser.add_argument('-i', '--iterations', required=False, type=int, default=30, help="The number of iterations [default=30]")
+parser.add_argument('-min', '--min', required=False, type=float, default=0.0, help="Minimum value while sampling [default=0.0]")
+parser.add_argument('-max', '--max', required=False, type=float, default=1.0, help="Maximum value while sampling [default=1.0]")
+args = parser.parse_args()
+
+# The interpreter works from the end to the beginning (reversed order)
+exps = [list(reversed(e.split())) for e in args.exps]
+
+attrNames = []
+for e in exps:
+   for t in e: # Accepts one of: ATTR, ATTRN, X, XN, VAR, VARN, V, VN; with N in [0,9]
+      if t.upper()=='ATTR' or (t.upper().startswith('ATTR') and len(t)>4 and t[4] in [str(i) for i in range(10)]):
+         attrNames.append(t)
+      elif t.upper()=='VAR' or (t.upper().startswith('VAR') and len(t)>3 and t[3] in [str(i) for i in range(10)]):
+         attrNames.append(t)
+      elif t.upper()=='X' or (t.upper().startswith('X') and len(t)>1 and t[1] in [str(i) for i in range(10)]):
+         attrNames.append(t)
+      elif t.upper()=='V' or (t.upper().startswith('V') and len(t)>1 and t[1] in [str(i) for i in range(10)]):
+         attrNames.append(t)
+
+
+attrNames = list(sorted(set(attrNames)))
+
 partial = []; temp = []; means = []; stds = []; none = {};
 for i in range(0, len(attrNames)):
    partial.append([])
@@ -167,12 +179,9 @@ for i in range(0, len(attrNames)):
    stds.append([])
    none[i] = 0
 
-# The interpreter works from the end to the beginning (reversed order)
-exps = [list(reversed(e)) for e in exps]
-
 count = 0
 for exp in exps:
-   for scenario in range(0,scenarios):
+   for scenario in range(0,args.scenarios):
       attr = {}; attr = {a : get_sample(a) for a in attrNames}
 
       value1 = interpreter(exp, attr)
@@ -182,7 +191,7 @@ for exp in exps:
             if a in exp:
                copy = dict.copy(attr)
          
-               for j in range(0,niter):
+               for j in range(0,args.iterations):
          
                   copy[a] = get_sample(a)
 
@@ -198,10 +207,10 @@ for exp in exps:
       else:
          for i, a in enumerate(attrNames):
             if a in exp:
-               none[i] += niter # all inner iterations are none (for each attribute)
+               none[i] += args.iterations # all inner iterations are none (for each attribute)
 
       count += 1
-      ProgressBar(count, scenarios*len(exps), out=sys.stderr)
+      ProgressBar(count, args.scenarios*len(exps), out=sys.stderr)
 
    for i in range(0,len(attrNames)):
       if temp[i]:
