aad061f7c03168846b192836a6493bb8cb2b5f0b
Douglas A. Augusto <daaugusto@gmail.com>
2017-04-01 13:12:02 -0300

    Refactor to allow for other methods.

--- script/feature_selection.py
+++ script/feature_selection.py
@@ -7,16 +7,19 @@ from numpy import linalg
 import subprocess, os, argparse, csv, tempfile, shutil
 
 
-def RunPEE(attr_ids, dataset, tmpdir):
+def RunPEE(index, training_data, training_header, tmpdir):
+   # Save the training dataset to a file (no need to save the header at this time, but doing it anyway)
+   np.savetxt(args.output, training_data, delimiter=",", header=','.join(training_header), comments='', fmt='%.8f')
+
    grammar = r"""   S = <exp>
    P = <exp> ::= <attribute> | <numeric_const> | <un_op> <exp> | <bin_op> <exp> <exp>
        <bin_op> ::= AND | OR | XOR | GREATER | GREATEREQUAL | LESS | LESSEQUAL | EQUAL | NOTEQUAL | ADD | SUB | MULT | DIV | MEAN | MAX | MIN | MOD | POW
        <un_op> ::= NOT | ABS | SQRT | POW2 | POW3 | POW4 | POW5 | NEG | ROUND | CEIL | FLOOR | EXP | EXP10 | EXP2 | LOG | LOG10 | LOG2 | SIN | COS | TAN | STEP | SIGN | LOGISTIC | GAMMA | GAUSS
        <numeric_const> ::= const | PI | PI_2 | PI_4 | 1_PI | 2_PI | 2_SQRTPI | SQRT2 | SQRT1_2 | E | LOG2E | LOG10E | LN2 | LN10 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | N1 | N2 | N3 | N4 | N5 | N6 | N7 | N8 | N9
        <attribute> ::= """
-   for j in range(len(attr_ids)-1):
+   for j in range(len(index)-1):
       grammar += "attr" + str(j)
-      if j < len(attr_ids)-2:
+      if j < len(index)-2:
          grammar += " | "
    grammar += '\n'
 
@@ -30,7 +33,7 @@ def RunPEE(attr_ids, dataset, tmpdir):
       with open(tmpdir+"/grammar.bnf", 'w') as g: g.write(grammar)
       os.system('cd ' + tmpdir + ' && cmake ' + args.pee + ' ' + args.cmake_args + ' -DGRAMMAR='+tmpdir+'/grammar.bnf -DLABEL=select -DCMAKE_BUILD_TYPE=RELEASE -DPROFILING=OFF > /dev/null && make -j -s')
 
-   mae = float(subprocess.check_output(tmpdir + "/select -d " + dataset + " " + args.pee_args + " | grep -a '^> [0-9]' | cut -d';' -f3", shell=True))
+   mae = float(subprocess.check_output(tmpdir + "/select -d " + args.output + " " + args.pee_args + " | grep -a '^> [0-9]' | cut -d';' -f3", shell=True))
 
    return mae
 
@@ -72,15 +75,18 @@ print "The temporary directory is: %s" % (TMPDIR)
 index = [0]
 rmaes = [np.inf]
 for i in range(1, nvar):
+   # Make 'i' the dependent variable (y), because we want to check if it can
+   # be expressed by the other features.
    index.append(i)
+
+   # Prepare the sliced training data (and header) to be used as regression
+   training_data = data[:,index]
+   training_header = []
    if args.has_header:
-      header_str = ','.join([h for j, h in enumerate(header) if j in index])
-   else:
-      header_str = ''
-   np.savetxt(args.output, data[:,index], delimiter=",", header=header_str, comments='', fmt='%.8f')
+      training_header = [h for j, h in enumerate(header) if j in index]
 
    #######################
-   mae = RunPEE(index, args.output, TMPDIR)
+   mae = RunPEE(index, training_data, training_header, TMPDIR)
    #######################
 
    if args.no_relative:
@@ -108,13 +114,13 @@ if args.has_y:
    index.append(len(data[0])-1)
 
 print ":: Excluded feature indices:", [j for j in range(0,nvar) if j not in index]
+
+training_header = []
 if args.has_header:
    print ":: Excluded feature names:", ','.join([h for j, h in enumerate(header) if j not in index])
-   header_str = ','.join([h for j, h in enumerate(header) if j in index])
-else:
-   header_str = ''
+   training_header = [h for j, h in enumerate(header) if j in index]
 
-np.savetxt(args.output, data[:,index], delimiter=",", header=header_str, comments='', fmt='%.8f')
+np.savetxt(args.output, data[:,index], delimiter=",", header=','.join(training_header), comments='', fmt='%.8f')
 
 # Remove temporary directory
 shutil.rmtree(TMPDIR, ignore_errors=True)
